a396bfc62dd1d00be24b91e234812d53fda65f73
Monique <monique.azevedo13022@gmail.com>
2021-04-17 12:32:42 -0300

    ajustes na SplashScreenActivity.kt

--- app/src/main/java/com/fulora/app/splash_screen/SplashScreenActivity.kt
+++ app/src/main/java/com/fulora/app/splash_screen/SplashScreenActivity.kt
@@ -2,18 +2,26 @@ package com.fulora.app.splash_screen
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.fulora.app.R
+import com.fulora.app.databinding.ActivitySplashScreenBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     val activityScope = CoroutineScope(Dispatchers.Main)
-//    lateinit var binding :
+    lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityScope.launch {
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+
+        activityScope.launch {
+            delay(1000)
+            //TODO start prox activity
         }
     }
 }
