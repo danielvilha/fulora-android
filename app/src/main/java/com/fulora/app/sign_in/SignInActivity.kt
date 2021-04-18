63d51f1e0fa3ff9a8ed94a1a2e55c6692fe93352
Monique <monique.azevedo13022@gmail.com>
2021-04-18 17:48:05 -0300

    lógica login

--- app/src/main/java/com/fulora/app/sign_in/SignInActivity.kt
+++ app/src/main/java/com/fulora/app/sign_in/SignInActivity.kt
@@ -3,6 +3,8 @@ package com.fulora.app.sign_in
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.fulora.app.R
@@ -14,12 +16,24 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
 
-        Spannable
+//        Spannable
     }
 
     companion object {
         fun createIntent(context: Context): Intent {
             return Intent(context, SignInActivity::class.java)
         }
+    }
+
+    fun login(nome: String, email: String, password: String) {
+        if (nome.isNotEmpty() && email.isEmailValid() && password.isNotEmpty()){
+
+        }else{
+            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun String.isEmailValid(): Boolean {
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
     }
 }
